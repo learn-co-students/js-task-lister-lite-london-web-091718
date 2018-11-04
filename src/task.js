65101cc233514cc084cdace48db8d7280669ff21
ssong-eu <sangyoon.song@gmail.com>
2018-11-04 15:41:35 +0000

    Done.

--- src/task.js
+++ src/task.js
@@ -1,2 +1,23 @@
 class Task {
+  constructor (task, list) {
+    this.id = Task.id()
+    this.description = task
+    this.list = list
+    this.list.tasks.push(this)
+  };
+
+  static id() {
+    if (!this.latestId) this.latestId = 1
+    else this.latestId++
+    return this.latestId
+  };
+
+  render() {
+    return `
+      <li>
+        ${this.description}
+        <button data-description="${this.description}">X</button>
+      </li>
+      `;
+  };
 }
